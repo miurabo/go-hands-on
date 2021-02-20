a91743a7d1935d5e15d12a5a87c14ff73a4ce9ee
Hiroyuki Miura <hmiura238@gmail.com>
2021-02-20 10:27:14 +0900

    feature: ログ機能の追加

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
+func LoggingSettings(logFile string) {
+	// RDWRはreadとwrite。パーミッションで0666は読み書きができるユーザーその他。
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
+}
+
 func main() {
+	LoggingSettings("test.log")
+	// テキトーなファイル名を開く処理
+	_, err := os.Open("iqsgfiquegf")
+	if err != nil {
+		log.Fatalln("Exit", err)
+	}
+
 	res, _ := http.Get("https://example.com")
 
 	fmt.Printf("[\x1b[32m%s\x1b[0m]\n", "OK   ")
 	fmt.Printf("[\x1b[31m%s\x1b[0m]\n", "   NG")
+
+	log.Println("aaa")
 
 	//fmt.Println(res.StatusCode)
 	fmt.Println(res.Proto)
